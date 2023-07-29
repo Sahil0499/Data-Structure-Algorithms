f837fa00324fdff9d54624c9fe0caf8531c1ca0c
Sahil Srivastava <sahilofficial0499@gmail.com>
2023-07-29 23:03:55 +0530

    practice

--- Array/ArraysCC.java
+++ Array/ArraysCC.java
@@ -1,4 +1,5 @@
 package Array;
+
 public class ArraysCC {
     public static void maxSubarraySum(int numbers[]) {
         int currSum = 0;
@@ -32,16 +33,16 @@ public class ArraysCC {
         int prefix[] = new int[numbers.length];
 
         prefix[0] = numbers[0];
-        //calculate prefix array
-        for(int i=1; i<prefix.length; i++) {
-            prefix[i] = prefix[i-1] + numbers[i];
+        // calculate prefix array
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
         }
 
         for (int i = 0; i < numbers.length; i++) {
             int start = i;
             for (int j = i; j < numbers.length; j++) {
                 int end = j;
-              
+
                 currSum = start == 0 ? prefix[end] : prefix[end] - prefix[end] - prefix[start - 1];
                 count++;
 
@@ -58,19 +59,20 @@ public class ArraysCC {
         int ms = Integer.MIN_VALUE;
         int cs = 0;
 
-        for(int i=0; i<numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             cs = cs + numbers[i];
-            if(cs<0) {
+            if (cs < 0) {
                 cs = 0;
             }
-            ms = Math.max(cs,ms);
+            ms = Math.max(cs, ms);
         }
-        System.out.println("our max subarray sum is : " +ms);
+        System.out.println("our max subarray sum is : " + ms);
     }
+
     public static void main(String args[]) {
         int numbers[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
         maxSubarraySum(numbers);
-         maxSubarraySum_usingPrefixArray(numbers);
-         Kadanes(numbers);
+        maxSubarraySum_usingPrefixArray(numbers);
+        Kadanes(numbers);
     }
 }
